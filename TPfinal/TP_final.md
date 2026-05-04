5bcffa99d4b135beb4a3672923aab23e829389c4
m-cbelanger <123122552+m-cbelanger@users.noreply.github.com>
2026-05-04 10:23:30 -0400

    TP final

---
+++ TPfinal/TP_final.md
@@ -0,0 +1,52 @@
+# Projet final (15%)
+
+Le projet est de faire une petite application qui résout des équations et fait des graphiques.
+
+Le code de l'interface est imposé, le modifier le moins possible. CERTAINS ÉLÉMENTS ONT ÉTÉ OMIS pour me permettre d'évaluer votre compréhension du transport des données et de la structuration du code. La fonction effacer_tout est disponible aussi et doit restée telle qu'elle est.
+
+
+
+
+
+Éléments essentiels:
+- L'application prend le texte d'équation et en fait la résolution. Le texte mal rentré NE FAIT PAS planter l'application, mais donne un avertissement et efface les entrées.
+- 2 options pour une équation ou la rencontre entre 2 équations. 
+    - Le bouton de type radiobutton doit être sélectionné pour indiquer on se trouve dans quelle situation
+    - Le bouton de type radiobutton doit se cocher tout seul si on sélectionne un espace pour entrer l'équation (voir exemple plus loin)
+- On peut spécifier une valeur initiale pour la recherche de solution avec la méthode de Newton-Raphson et lui mettre une valeur par défaut. La méthode de Newton DOIT apparaître quelque part dans la résolution.
+- La résolution doit fonctionner pour tous les éléments mathématiques de cette liste: +,-,*,/,sin, cos, tan, exp, valeur de pi. Les autres manipulations ne seront pas testées.
+-Il doit y avoir un bouton pour regarder le graphique de l'équation seule ou le graphique des 2 équations ensemble, selon le radiobutton coché. 
+- Il doit y avoir l'option d'effacer tous les champs, ce qui remet une valeur initiale par défaut.
+- Porter attention à la disposition et la facilité d'utilisation de l'application.
+- Chaque fonction doit fonctionner indépendamment du module dans lequel elle se trouve (donc paramètres obligatoires, pas de variables globales)
+
+Voici le fonctionnement attendu:
+
+![](img/TPfinal.gif)
+
+
+Les fonctions suivantes seront testées:
+
+```py
+"""
+1 équation:
+- 3  aucune solution
+- 3*x + 6   réponse -2
+- 3*x**2-5*x-20  2 réponses
+- 3*x^2-5*x-9  fonctionne aussi avec ^
+- sin(x) - 4*pi    Aucune solution
+- 2*sin(x) - pi/2    réponse: 0.9033 (et autres)
+
+- exp(x+9)-8  réponse 6,9206
+- 3/x +9  réponse de -0.3333
+
+2 équations
+- cos(x) et x  réponse 0.7391
+- cos(x) et exp(x)  réponses multiples 
+"""
+```
+
+# Plan pour préparer ce travail.
+
+Pour réussir un petit projet informatique, il est essentiel de bien faire le plan. Plusieurs sections sont décelables dans ce projet, ils constituent donc l'occasion de faire des modules.
+
---
+++ TPfinal/interface_initial.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+
+def effacer_tout(form1, form2,form3,form4,label1,y_min,y_max):
+    form1.delete(0, tk.END)
+    form2.delete(0, tk.END)
+    form3.delete(0, tk.END)
+    form4.delete(0, tk.END)
+    form4.insert(0,"0")
+    label1.config(text = "")
+    y_min.delete(0, tk.END)
+    y_max.delete(0, tk.END)
+    y_min.insert(-10,"-10")
+    y_max.insert(10,"10")
+
+
+
+def affichage_fenetre():
+    
+    fenetre = tk.Tk()
+    fenetre.title("Résolution d'équations")
+    fenetre.geometry("500x350")
+
+    choix = tk.IntVar(value=1)
+
+    # Les 3 Entry où entrer les équations
+    form1 = tk.Entry(fenetre, width=30)
+    form2 = tk.Entry(fenetre, width=30)
+    form3 = tk.Entry(fenetre, width=30)
+
+    # Les 2 endroits où modifier le maximum et minimum en y
+    ymin_label = tk.Label(fenetre, text="y min")
+    ymax_label = tk.Label(fenetre, text="y max")
+    ymin_entry = tk.Entry(fenetre, width=10)
+    ymax_entry = tk.Entry(fenetre, width=10)
+    ymin_entry.insert(0, "-10")
+    ymax_entry.insert(0, "10")
+
+    # La valeur initiale pour la résolution d'équations avec méthode de Newton
+    form4 = tk.Entry(fenetre, width=10)
+    label4 = tk.Label(fenetre, text="Valeur initiale", width=15, height=2)
+    form4.insert(0, "0")
+    
+    # Pour afficher les réponses
+    label1 = tk.Label(fenetre, text="")
+    
+    # Bouton radio pour indiquer si on a une ou 2 équations (se sélectionne dès qu'on clique dans l'une des Entry en-dessous
+    radio1 = tk.Radiobutton(fenetre, text="Une équation", variable=choix, value=1)
+    radio2 = tk.Radiobutton(fenetre, text="Deux équations", variable=choix, value=2)
+    # Obliger le radio bouton à changer selon la boîte de texte choisie.
+    form1.bind("<FocusIn>", lambda event: choix.set(1))
+    form2.bind("<FocusIn>", lambda event: choix.set(2))
+    form3.bind("<FocusIn>", lambda event: choix.set(2))
+    
+    # Étiquette invisible dans laquelle apparaîtra la réponse
+    reponse = tk.Label(fenetre, text="")
+
+    # boutons qui déclenchent les actions de résoudre la ou les équations, créer le graphique ou effacer tout.
+    # Aucune action associée pour le moment
+    btn1 = tk.Button(fenetre, text="Résoudre", width=10, height=2)
+    btn2 = tk.Button(fenetre, text="Graphique", width=10, height=2)
+    btn3 = tk.Button(fenetre, text="Effacer tout", width=10, height=2,
+                     command=lambda: effacer_tout(form1, form2, form3, form4, label1,ymin_entry,ymax_entry))
+
+    # Disposition des objets dans la fnêtre
+    fenetre.grid_columnconfigure(0, weight=1)
+    fenetre.grid_columnconfigure(1, weight=1)
+    fenetre.grid_columnconfigure(2, weight=1)
+
+    radio1.grid(row=0, column=0, sticky="w", padx=10, pady=5)
+    radio2.grid(row=0, column=1, sticky="w", padx=5, pady=5)
+
+    form1.grid(row=1, column=0, padx=10, pady=5)
+    form2.grid(row=1, column=1, padx=5, pady=5)
+
+    ymin_label.grid(row=2, column=0, sticky="w", padx=10)
+    ymin_entry.grid(row=2, column=0, padx=(70, 5), pady=2)
+
+    form3.grid(row=2, column=1, padx=5, pady=5)
+
+    ymax_label.grid(row=3, column=0, sticky="w", padx=10)
+    ymax_entry.grid(row=3, column=0, padx=(70, 5), pady=2)
+
+    label4.grid(row=4, column=0, padx=5, pady=10, sticky="w")
+    form4.grid(row=4, column=1, padx=5, pady=10)
+
+    reponse.grid(row=5, columnspan=3, padx=10, pady=10, sticky="ew")
+
+    btn1.grid(row=6, column=0, padx=10, pady=10, sticky="ew")
+    btn2.grid(row=6, column=1, padx=10, pady=10, sticky="ew")
+    btn3.grid(row=7, column=0, padx=10, pady=10, sticky="ew")
+
+    fenetre.mainloop()
+    
+# Appel de fonction à déplacer
+affichage_fenetre()
